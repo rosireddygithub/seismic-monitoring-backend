4e58ab45f4280065e948c2cfda5d89d2fa03cbaf
Venkata Rosi Reddy Jakkireddy <161752906+rosireddygithub@users.noreply.github.com>
2025-06-16 15:43:42 +0530

    Update app.py

--- server/app.py
+++ server/app.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
+import os
 
 app = Flask(__name__)
 CORS(app)
@@ -17,4 +18,5 @@ def get_status():
     return jsonify(statuses)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
